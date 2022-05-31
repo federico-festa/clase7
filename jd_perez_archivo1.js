8b08bea0294da231f61883018eee3dea8cf22e14
JD_PerezN <jd.perez998@gmail.com>
2022-05-30 19:25:51 -0500

    updated archivo1 nuevas lineas

--- jd_perez_archivo1.js
+++ jd_perez_archivo1.js
@@ -1,5 +1,8 @@
 let suma2Numeros = (a, b) => (a + b)
+let otraFuncion = a => a
 
 module.exports = suma2Numeros
 
 // un comment de nacho en el archivo de julian
+
+// Nuevo comentario adicionado al archivo... 
