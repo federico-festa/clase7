4fe525f1921620e26a69fa06d55454b38488d490
nacho <nacho@nachos-MacBook-Pro.local>
2022-05-30 21:30:19 -0300

    hola

--- jd_perez_archivo1.js
+++ jd_perez_archivo1.js
@@ -5,8 +5,4 @@ module.exports = suma2Numeros
 
 // un comment de nacho en el archivo de julian
 
-<<<<<<< HEAD
 // esta es la edición del punto 7 (trabajar en el mismo archivo), por ignacio. 
-=======
-// Nuevo comentario adicionado al archivo... 
->>>>>>> 8b08bea0294da231f61883018eee3dea8cf22e14
